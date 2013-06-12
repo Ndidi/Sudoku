3a77e406aaddb55570cfaf306e9163315fca3582
Alex Norton <sandig@gmail.com>
2013-06-12 09:25:11 +0100

    Sudoku generation, entering of numbers in empty squares and error checking

--- static/script/appui/components/game.js
+++ static/script/appui/components/game.js
@@ -4,7 +4,8 @@ require.def("sudoku/appui/components/game",
 		"antie/widgets/label",
 		"antie/widgets/grid",
 		"antie/widgets/button",
-		"antie/events/keyevent"
+		"antie/events/keyevent",
+		"sudoku/lib/sudoku"
 	],
 	function (Component, Label, Grid, Button, KeyEvent) {
 		return Component.extend({
@@ -17,28 +18,86 @@ require.def("sudoku/appui/components/game",
 
 				this._gameGrid = new Grid("gamegrid", 9, 9);
 
-				for(var i = 0; i < 9; i++) {
-					for(var j = 0; j < 9; j++) {
-						this._gameGrid.setWidgetAt(i, j, new Button());
-						this._gameGrid.getWidgetAt(i, j).appendChildWidget(new Label("0"));
+				for(var row = 0; row < 9; row++) {
+					for(var col = 0; col < 9; col++) {
+						var button = new Button();
+
+						button._row = row;
+						button._col = col;
+
+						button.appendChildWidget(new Label("label_" + row + "_" + col, "&nbsp"));
+
+						this._gameGrid.setWidgetAt(col, row, button);
 					}
 				}
 
 				this.appendChildWidget(this._gameGrid);
 
 				this.addEventListener('keydown', function(e) { self._onKeyDown(e); });
+
+				this._sudoku = new Sudoku();
+				this._sudoku.level = 2;
+
+				this._sudoku.done = function() { self._updateGrid(); }
+
+				this._sudoku.newGame();
 	
 			},
 
 			_onKeyDown: function(e) {
-				// Only handle numeric keypresses
-				if(e.keyCode >= KeyEvent.VK_0 && e.keyCode <= KeyEvent.VK_9) {
+				// Only allow digits 1-9 to be entered
+				if(e.keyCode >= KeyEvent.VK_1 && e.keyCode <= KeyEvent.VK_9) {
 					e.stopPropagation();
-					this._gameGrid._activeChildWidget._childWidgetOrder[0].setText(e.keyChar);
+
+					var activeButton = this._gameGrid._activeChildWidget;
+
+					if(!activeButton.hasClass("hint")) {
+						activeButton._childWidgetOrder[0].setText(e.keyChar);
+						this._sudoku.setVal(activeButton._row, activeButton._col, parseInt(e.keyChar));
+
+
+						this._showErrors();
+					}
 				} else {
 					return;
 				}
+			},
+
+			_updateGrid: function() {
+				for(var row = 0; row < 9; row++) {
+					for(var col = 0; col < 9; col++) {
+						var squareValue = this._sudoku.getVal(row, col);
+						
+						if(squareValue != "0") {
+							this._gameGrid.getWidgetAt(col, row)._childWidgetOrder[0].setText(squareValue);
+							this._gameGrid.getWidgetAt(col, row).addClass("hint");
+						}
+					}
+				}
+			},
+
+			_showErrors: function() {
+				for(var row = 0; row < 9; row++) {
+					for(var col = 0; col < 9; col++) {
+						
+						var button = this._gameGrid.getWidgetAt(col, row);
+
+						var value = this._sudoku.getVal(row, col);
+
+						if(this._sudoku.checkVal(row, col, value) == false) {
+							button.addClass("error");
+						} else {
+							button.removeClass("error");
+						}
+					}
+				}
+			},
+
+			_getActiveSquareCoords: function() {
+
 			}
+
+
 		});
 	}
 );
